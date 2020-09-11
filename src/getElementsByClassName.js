3b3acdcc48078407da1a9f248eabac9ca1c64415
Tyna Nguyen <ngtyna@gmail.com>
2020-09-11 10:06:38 -0500

    Write pseudocode to begin implementing getElementsByClassName

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -5,5 +5,28 @@
 
 // But instead we're going to implement it from scratch:
 var getElementsByClassName = function(className) {
-  // your code here
+  // console.log(document);
+
+  // define a result array to contain child elements of given class name
+
+  // define inner function to check the class name
+  var checkClassName = function() {
+    // check if the body element has a class
+    // check if the class is equal to className
+      // add the body element to results
+
+    // check if the body element has a child node
+      // iterate through each child node to check the class name
+        // call checkClassName recursively on current child node
+  };
+
+  // invoke inner function
+  // return result array
 };
+
+/*
+(MDN) Document Body Element
+- the HTML body element represents the content of an HTML document
+- document.body property represents the body node of the current document
+- document.body returns the body element
+*/
